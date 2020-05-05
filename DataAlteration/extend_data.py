cdeae4bef5bb9db239c5cba658b3548b64300594
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-05 16:38:46 -0500

    Fixed recrop bounds Adjusted global variable usage

--- DataAlteration/extend_data.py
+++ DataAlteration/extend_data.py
@@ -5,8 +5,8 @@ import cv2
 from datetime import datetime
 
 #Distortion parameter limits 
-MIN_WIDTH = 100
-MIN_HEIGHT = 100
+MIN_WIDTH = 1000
+MIN_HEIGHT = 1000
 MAX_NOISE = 10
 MAX_SKEW = 8 
 MAX_COLOUR_DISTORT = 10 
@@ -17,7 +17,7 @@ NOISE_FREQ = 2
 SKEW_FREQ = 2
 COLOUR_DISTORT_FREQ = 2
 
-DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\Raw Data"
+DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\Raw Data\*.jpg"
 now = datetime.now()
 dt_string = now.strftime("%d.%m.%Y %H-%M-%S")
 OUTPUT_PATH = "C:\\Users\\User\\Documents\\Hylife 2020\\Loin Feeder\\Data\\Output Data "+dt_string
@@ -49,27 +49,31 @@ def main(input_path=DATA_PATH, output_path=OUTPUT_PATH, resize_freq=RESIZE_FREQ,
         "\n\tColour distortion frequency:", colour_distort_freq)
 
     for item_name in in_data:
+        print("Viewing",item_name)
         try:
-            output = cv2.imread(item_name)
+            og = cv2.imread(item_name)
         except:
             print("ERR: Item",item_name,"not an image.")
             continue
+        else:
+            print("Image detected.")
 
-        export(output, counter, output_path)
+        export(og, counter, output_path)
         counter += 1 
 
         for i in range(0, 10):
+            output = og
             if (r.randint(0, resize_freq-1) == 0):
                 output = recrop(output)
-            if (r.randint(0, noise_freq-1) == 0):
-                output = make_noise(output)
-            if (r.randint(0, skew_freq-1) == 0):
-                output = skew(output, "h")
-            if (r.randint(0, skew_freq-1) == 0):
-                output = skew(output, "v")
-            if (r.randint(0, colour_distort_freq-1) == 0):
-                output = colour_distort(output)
-            output = reduce_quality(output)
+            # if (r.randint(0, noise_freq-1) == 0):
+            #     output = make_noise(output)
+            # if (r.randint(0, skew_freq-1) == 0):
+            #     output = skew(output, "h")
+            # if (r.randint(0, skew_freq-1) == 0):
+            #     output = skew(output, "v")
+            # if (r.randint(0, colour_distort_freq-1) == 0):
+            #     output = colour_distort(output)
+            # output = reduce_quality(output)
 
             export(output, counter, output_path)
             counter += 1
@@ -77,15 +81,17 @@ def main(input_path=DATA_PATH, output_path=OUTPUT_PATH, resize_freq=RESIZE_FREQ,
     print("Dataset extended.")
 
 def recrop(img, min_width=MIN_WIDTH, min_height=MIN_HEIGHT):
-    iH, iW = img.shape
+    iH, iW, iD = img.shape
     
     #Chooses random size within constraints
-    width = r.randint(min_width, iW)
-    height = r.randint(min_height, iH)
+    width = r.randint(min_width, iW-1)
+    height = r.randint(min_height, iH-1)
 
     #Chooses random offsets based off new size
     height_offset = r.randint(0, iH - height)
     width_offset = r.randint(0, iW - width) 
+
+    print("new bounds:\n\t", height_offset,":",height_offset+height,"\n\t",width_offset,":",width_offset+width)
 
     return img[height_offset:height_offset+height, width_offset:width_offset+width]
 
@@ -102,7 +108,7 @@ def colour_distort(img, max_colour_distort=MAX_COLOUR_DISTORT):
     pass
 
 def export(img, counter, output_path):
-    pass
+    cv2.imwrite(output_path + "\\" + str(counter) + ".png",img)
 
 if __name__=='__main__':
     main()
