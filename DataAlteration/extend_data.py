1a98232150e7055f8a109365dd5e4d584890acec
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-05 16:10:49 -0500

    Established output directories and error checking

--- DataAlteration/extend_data.py
+++ DataAlteration/extend_data.py
@@ -1,12 +1,15 @@
 import os
 import glob 
 import random as r
+import cv2
+from datetime import datetime
 
 #Distortion parameter limits 
 MIN_WIDTH = 100
 MIN_HEIGHT = 100
 MAX_NOISE = 10
 MAX_SKEW = 8 
+MAX_COLOUR_DISTORT = 10 
 MAX_QUALITY_REDUCTION = 25
 
 RESIZE_FREQ = 2
@@ -15,7 +18,9 @@ SKEW_FREQ = 2
 COLOUR_DISTORT_FREQ = 2
 
 DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\Raw Data"
-OUTPUT_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\Output Data"
+now = datetime.now()
+dt_string = now.strftime("%d.%m.%Y %H-%M-%S")
+OUTPUT_PATH = "C:\\Users\\User\\Documents\\Hylife 2020\\Loin Feeder\\Data\\Output Data "+dt_string
 
 def main(input_path=DATA_PATH, output_path=OUTPUT_PATH, resize_freq=RESIZE_FREQ, noise_freq=NOISE_FREQ, skew_freq=SKEW_FREQ, colour_distort_freq=COLOUR_DISTORT_FREQ):
     '''This function extends a data set for training purposes. 
@@ -29,14 +34,31 @@ def main(input_path=DATA_PATH, output_path=OUTPUT_PATH, resize_freq=RESIZE_FREQ,
     in_data = glob.glob(input_path)
     counter = 0
 
-    for item in in_data:
-        print("Item:", item, "\n")
-        export(item, counter)
+    try:
+        os.mkdir(output_path)
+    except:
+        print("ERR: Invalid output path.")
+    else:
+        print("Output folder created successfully.")
+
+    print("Extending dataset...\n\nInput path:",
+        input_path,"\nOutput path:", output_path, 
+        "\n\nDistortion Parameters:\n\tResize frequency:",
+        resize_freq, "\n\tNoise frequency:",noise_freq, 
+        "\n\tSkew frequency:", skew_freq,
+        "\n\tColour distortion frequency:", colour_distort_freq)
+
+    for item_name in in_data:
+        try:
+            output = cv2.imread(item_name)
+        except:
+            print("ERR: Item",item_name,"not an image.")
+            continue
+
+        export(output, counter, output_path)
         counter += 1 
 
         for i in range(0, 10):
-            output = item
-
             if (r.randint(0, resize_freq-1) == 0):
                 output = recrop(output)
             if (r.randint(0, noise_freq-1) == 0):
@@ -46,11 +68,13 @@ def main(input_path=DATA_PATH, output_path=OUTPUT_PATH, resize_freq=RESIZE_FREQ,
             if (r.randint(0, skew_freq-1) == 0):
                 output = skew(output, "v")
             if (r.randint(0, colour_distort_freq-1) == 0):
-                output = colour_distort(output, "v")
+                output = colour_distort(output)
             output = reduce_quality(output)
 
-            export(output, counter)
+            export(output, counter, output_path)
             counter += 1
+
+    print("Dataset extended.")
 
 def recrop(img, min_width=MIN_WIDTH, min_height=MIN_HEIGHT):
     pass
@@ -64,7 +88,10 @@ def skew(img, orientation, max_skew=MAX_SKEW):
 def reduce_quality(img, max_quality_reduction=MAX_QUALITY_REDUCTION):
     pass
 
-def export(img, counter):
+def colour_distort(img, max_colour_distort=MAX_COLOUR_DISTORT):
+    pass
+
+def export(img, counter, output_path):
     pass
 
 if __name__=='__main__':
