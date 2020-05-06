d9b077826da741c2633efd24ac88a0cd5fab72c1
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-06 11:23:29 -0500

    Added bbox file Began working on altering code to fit need. Removed bounding circles Began splitting file into callable functions.

---
+++ VisionIdentification/bbox.py
@@ -0,0 +1,36 @@
+'''Altered from bbox example on opencv documentation site 
+https://docs.opencv.org/3.4/da/d0c/tutorial_bounding_rects_circles.html 
+'''
+
+import numpy as np
+import cv2
+import argparse 
+import random as r 
+
+r.seed(12345)
+
+def thresh_callback(val):
+    threshold = val #Threshold value for contours 
+    canny_output = cv2.Canny(src_gray, threshold, threshold * 2)
+    
+    _, contours, _ = cv2.findContours(canny_output, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    contours_poly = [None]*len(contours)
+    boundRect = [None]*len(contours)
+    for i, c in enumerate(contours):
+        contours_poly[i] = cv2.approxPolyDP(c, 3, True)
+        boundRect[i] = cv2.boundingRect(contours_poly[i])
+    
+    
+    drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
+    
+    
+    for i in range(len(contours)):
+        color = (r.randint(0,256), r.randint(0,256), r.randint(0,256))
+        cv2.drawContours(drawing, contours_poly, i, color)
+        cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])), \
+          (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
+    
+    
+    cv.imshow('Contours', drawing)
+    
