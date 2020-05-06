24a9f7eef1f87a33958da71f4fcdff03f998e4a2
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-06 13:50:40 -0500

    Vision preprocessing (#3)
    * Creation of extend data script
    
    * Established output directories and error checking
    
    * Added recrop function
    
    * Fixed recrop bounds
    Adjusted global variable usage
    
    * Added noise function
    
    * Added skew function
    
    * Misc changes
    Added quality reduction
    Changed recrop parameters to be as a ratio
    Tuned noise parameters
    
    * Added lighting distortion
    
    * Added preprocessing file
    Created a square crop function to unify input image shape
    
    * Added scale function
    
    * Renamed preprocessing, fixed bugs
    
    * Added bbox file
    Began working on altering code to fit need. Removed bounding circles
    Began splitting file into callable functions.
    
    * Enhanced bbox functionality.
    Removed blurring
    Adjusted threshold
    Finished converting useful code from tutorial
    
    * Found single bbox
    Determined bbox based off contour's bboxes variation from boundrect mean
    Used smallest bbox found
    
    * Documented code
    Added comments
    Fixed outlier filter code to include Y.
    Re-ordered document structure.

---
+++ VisionIdentification/image_sizing.py
@@ -0,0 +1,42 @@
+import numpy as np
+import cv2
+
+DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\Raw Data\*.jpg"
+DEST_WIDTH = 500
+DEST_HEIGHT = 500
+
+def main(input_path=DATA_PATH):
+    import glob
+    
+    in_data = glob.glob(input_path)
+
+    for item_name in in_data:
+        try:
+            og = cv2.imread(item_name)
+        except:
+            continue
+
+        test1 = crop(og) 
+        test1 = scale(test1) 
+
+        test2 = scale(og)
+
+        output_path = "C:\\Users\\User\\Documents\\Hylife 2020\\Loin Feeder\\Data"
+
+        cv2.imwrite(output_path + "\\test1.png",test1)
+        cv2.imwrite(output_path + "\\test2.png",test2)
+
+def crop(img):
+    '''Crops image to be largest square possible'''
+    iH, iW, iD = img.shape
+    crop_size = min(iH, iW)
+    ret = img[(iH - crop_size)//2:(iH - crop_size)//2 + crop_size,(iW - crop_size)//2:(iW - crop_size)//2+crop_size]
+    return ret
+
+def scale(img, dest_height=DEST_HEIGHT, dest_width=DEST_WIDTH):
+    iH, iW, iD = img.shape
+    res = cv2.resize(img, dsize=(dest_height, dest_width), interpolation=cv2.INTER_CUBIC)
+    return res
+
+if __name__=='__main__':
+    main()
