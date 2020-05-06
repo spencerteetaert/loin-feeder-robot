76d28c171163814a41b83041601ca22b6f12e89b
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-06 12:45:43 -0500

    Found single bbox Determined bbox based off contour's bboxes variation from boundrect mean Used smallest bbox found

--- VisionIdentification/bbox.py
+++ VisionIdentification/bbox.py
@@ -10,7 +10,7 @@ import image_sizing as imgsz
 
 r.seed(12345)
 
-DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\right middle.jpg"
+DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\left middle.jpg"
 THRESHOLD = 130
 
 def show(img):
@@ -34,10 +34,10 @@ def thresh_callback(val, temp):
 def draw_results(img, canny_output, contours_poly, contours, boundRect):
     drawing = np.zeros((canny_output.shape[0], canny_output.shape[1], 3), dtype=np.uint8)
     
-    for i in range(len(contours)):
+    for i in range(len(boundRect)):
         color = (r.randint(0,256), r.randint(0,256), r.randint(0,256))
-        cv2.drawContours(drawing, contours_poly, i, color)
-        cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])), \
+        # cv2.drawContours(drawing, contours_poly, i, color)
+        cv2.rectangle(drawing, (int(boundRect[i][0]), int(boundRect[i][1])), 
           (int(boundRect[i][0]+boundRect[i][2]), int(boundRect[i][1]+boundRect[i][3])), color, 2)
     
     cv2.imshow('Original Image', img)
@@ -50,16 +50,51 @@ def preprocess(img):
 
     # Convert image to gray and blur it -- removes noise
     ret[:,:,2] = np.zeros([ret.shape[0], ret.shape[1]])
-    # show(ret)
     ret = cv2.cvtColor(ret, cv2.COLOR_BGR2GRAY)
-    # show(ret)
     # ret = cv2.blur(ret, (3,3))
-    # show(ret)
     return ret 
+
+def filter_outliers(boundRect, m=1):
+    centersX = [int(boundRect[i][0]+boundRect[i][2]/2) for i in range(0, len(boundRect))]
+    centersY = [int(boundRect[i][1]+boundRect[i][3]/2) for i in range(0, len(boundRect))]
+
+    avgX, stdX = np.average(centersX), np.std(centersX)
+    avgY, stdY = np.average(centersY), np.std(centersY)
+
+    filt = []
+
+    for i in range(0, len(boundRect)):
+        if (abs(centersX[i] - avgX) < stdX * m):
+            filt+=[True]
+        else:
+            filt+=[False]
+
+    ret = [boundRect[i] for i in range(0, len(boundRect)) if filt[i]==True]
+    return ret
+
+def find_large_bbox(boundRects):
+    minX = 100000
+    maxX = 0
+    minY = 100000
+    maxY = 0
+    
+    for i in range(0, len(boundRects)):
+        if boundRects[i][0] < minX:
+            minX = boundRects[i][0]
+        if boundRects[i][1] < minY:
+            minY = boundRects[i][1]
+        if boundRects[i][0]+boundRects[i][2] > maxX:
+            maxX = boundRects[i][0]+boundRects[i][2]
+        if boundRects[i][1]+boundRects[i][3] > maxY:
+            maxY = boundRects[i][1]+boundRects[i][3]
+    
+    return [(minX, minY, maxX - minX, maxY - minY)]
 
 def get_bbox(img, threshold=THRESHOLD, draw=False):
     temp = preprocess(img)
     boundRect, contours_poly, canny_output, contours = thresh_callback(threshold, temp)
+    boundRect = filter_outliers(boundRect)
+    boundRect = find_large_bbox(boundRect)
 
     if draw:
         draw_results(temp, canny_output, contours_poly, contours, boundRect)
