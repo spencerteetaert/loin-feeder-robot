f3fd6f97588ab8bfbaef151a0aeed933751d08bc
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-06 09:16:12 -0500

    Added scale function

--- VisionIdentification/preprocessing.py
+++ VisionIdentification/preprocessing.py
@@ -3,6 +3,9 @@ import cv2
 import glob
 
 DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\Raw Data\*.jpg"
+DEST_WIDTH = 400
+DEST_HEIGHT = 400
+
 
 def main(input_path=DATA_PATH):
     in_data = glob.glob(input_path)
@@ -13,13 +16,15 @@ def main(input_path=DATA_PATH):
         except:
             continue
 
-        print(og.shape)
-        test = crop(og) 
-        print(test.shape)
+        test1 = crop(og) 
+        test1 = scale(test1) 
 
-        cv2.imshow("test",test)
-        cv2.waitKey(0)
+        test2 = scale(og)
 
+        output_path = "C:\\Users\\User\\Documents\\Hylife 2020\\Loin Feeder\\Data"
+
+        cv2.imwrite(output_path + "\\test1.png",test1)
+        cv2.imwrite(output_path + "\\test2.png",test2)
 
 def crop(img):
     '''Crops image to be largest square possible'''
@@ -27,6 +32,11 @@ def crop(img):
     crop_size = min(iH, iW)
     ret = img[(iH - crop_size)//2:(iH - crop_size)//2 + crop_size,(iW - crop_size)//2:(iW - crop_size)//2+crop_size]
     return ret
+
+def scale(img):
+    iH, iW, iD = img.shape
+    res = cv2.resize(img, dsize=(DEST_HEIGHT, DEST_WIDTH), interpolation=cv2.INTER_CUBIC)
+    return res
 
 if __name__=='__main__':
     main()
