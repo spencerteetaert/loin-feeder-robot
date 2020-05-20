28bd5d9170a62ae50ff8a9fcb2369b2a9d942711
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-20 16:44:06 -0500

    Removed mains from sample code

--- tools/from_vid.py
+++ tools/from_vid.py
@@ -1,10 +1,13 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import time
 
 import numpy as np 
 import cv2
 
-from . import bbox
-from . import meat 
+from sample.VisionIdentification import bbox
+from sample.VisionIdentification.meat import Meat 
 
 DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\good.mp4"
 
