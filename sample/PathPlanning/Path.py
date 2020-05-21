9289deec3377958836bbd84d3f7e3099511b6589
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-21 15:16:55 -0500

    Added angle component to planned path

--- sample/PathPlanning/Path.py
+++ sample/PathPlanning/Path.py
@@ -3,28 +3,21 @@ import numpy as np
 from ..Model.Point import Point
 from .. import GlobalParameters
 
-class Path:
+class PathFinder:
     def __init__(self, environment):
         self.environment = environment
 
     def __call__(self, start_point, end_point, speed):
-        '''
-            Given a pre-set list of finite lines that are safe to travel
-            1) calculate line of length speed between the current point and the destination
-            2) at the tip of that line, find the nearest point on a "safe line". 
-                This is the next point
-            3) Find another line of length speed from position on safe line and repeat this process 
-                until the end point is within speed of the current point 
-        '''
         end_p = end_point.toArray()
+        start_p = start_point.toArray()
         ret = []
         ret += [start_point]
 
-        current = start_point.toArray()
+        current = start_p + (end_p - start_p)/np.sqrt((end_p - start_p).dot(end_p - start_p))*speed
+
         fail_safe = 0
         norm = speed + 1
         while norm > speed:
-            #Do something 
             dists = []
             closest_point = 0
             for i in range(0, len(self.environment)):
@@ -38,7 +31,10 @@ class Path:
                 dists += [dist]
                 if min(dists) == dist:
                     closest_point = a + t*n
-            ret += [Point(closest_point[0], closest_point[1])]
+
+            temp = Point(closest_point[0], closest_point[1])
+            if (temp - ret[-1]).mag() > speed:
+                ret += [temp]
 
             norm = np.sqrt((end_p - closest_point).dot(end_p - closest_point))
             current = closest_point + (end_p - closest_point)/norm*speed
@@ -49,5 +45,16 @@ class Path:
                 break            
 
         ret += [end_point]
+
+        diff = end_point.angle - start_point.angle
+        if diff > 180:
+            dA = -1*(360 - diff)/(len(ret) - 2)
+        elif diff < -180:
+            dA = -1*(360 + diff)/(len(ret) - 2)
+        else:
+            dA = diff/(len(ret) - 2)
+
+        for i in range(1, len(ret)-1):
+            ret[i].angle = start_point.angle + dA*i
 
         return ret
