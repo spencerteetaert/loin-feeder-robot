6750c4b32888959cc22add83db88f9a5e4b39c85
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-05-21 13:43:40 -0500

    Added path class

---
+++ sample/PathPlanning/Path.py
@@ -0,0 +1,13 @@
+class Path:
+    def __init__(self):
+        self.start_point = 0
+        self.end_point = 0
+        self.in_between_points = 0 
+        
+        pass
+
+    def __repr__(self):
+        pass
+
+    def gen_inbetween_points(self):
+        pass
