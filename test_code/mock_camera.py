fb1efc79ef453b6ac501c8b7016af8a1c8152d31
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-06-02 15:42:29 -0500

    Continued dev on mock files

---
+++ test_code/mock_camera.py
@@ -0,0 +1,110 @@
+import time
+
+import numpy as np 
+import cv2
+
+from ..vision_identification import bounding_box
+from ..vision_identification.video_reader import FileVideoStream
+from ..vision_identification import meat
+from ..model.robot import Robot
+from ..model.point import Point
+from ..path_planning.path_runner import PathRunner
+from ..path_planning import graphing_tools
+from .. import global_parameters
+
+DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\good.mp4"
+DISPLAY_TOGGLE = True
+
+streamer = FileVideoStream(DATA_PATH)
+streamer.start()
+time.sleep(1) 
+
+def main(data_path=DATA_PATH):
+    global streamer
+    # out = cv2.VideoWriter(r'C:\Users\User\Documents\Hylife 2020\Loin Feeder\output.mp4', 0x7634706d, 30, (1680,830))
+
+    if DISPLAY_TOGGLE:
+        win = "Window"
+        cv2.namedWindow(win)
+
+    delay = 0
+    flip_flop = False 
+    flip_flop2 = False
+
+    meats = [0]
+    queue1 = []
+    queue2 = []
+    times = []
+
+    while(streamer.running):
+        start = time.time()
+        ################################################
+        ### Video Processing and Meat Identification ###
+        ################################################
+        
+        force_timer = time.time()
+
+        # Keeps streamer queue size within a lag free range (>0 and <128)
+        qsize = streamer.Q.qsize()
+        if qsize < 40:
+            streamer.sleep_time = 0
+        elif qsize > 88:
+            streamer.sleep_time = 0.005
+
+        temp = streamer.read()
+        frame = bounding_box.scale(temp)
+        frame = cv2.copyMakeBorder(frame, 0, 300, 300, 300, cv2.BORDER_CONSTANT, value=0)
+
+        iH, iW, iD = frame.shape
+        box, _, _ = bounding_box.get_bbox(frame)
+        
+        # Artificially simulate camera trigger 
+        if (box != 0):
+            for i in range(0, len(box)):
+                if delay > 50:
+                    cX = int(box[i][1]["m10"] / box[i][1]["m00"])
+                    cY = int(box[i][1]["m01"] / box[i][1]["m00"])
+
+                    if iH / 3 - 5 < cY and iH / 3 + 5 > cY:
+                        if flip_flop:
+                            meats += [meat.Meat(box[i], side="Right", center=[cX, cY])]
+                        else:
+                            meats += [meat.Meat(box[i], side="Left", center=[cX, cY])]
+                        flip_flop = not flip_flop
+                        delay = 0
+
+        delay += 1
+
+        ###############
+        ### Display ###
+        ###############        
+        
+        if DISPLAY_TOGGLE:
+            cv2.imshow(win, frame)
+
+        ################
+        ### Controls ###
+        ################
+
+        for i in range(1, len(meats)):
+            meats[i].step()
+
+        if DISPLAY_TOGGLE:
+            k = cv2.waitKey(1) & 0xFF
+            if k == ord('q'):
+                break
+            elif k == ord('p'):
+                cv2.waitKey(0)
+            
+        times += [time.time() - start]
+        # out.write(frame)
+
+        #Artifically slow the program to the desired frame rate
+        # cv2.waitKey(max(global_parameters.FRAME_RATE - round((time.time() - force_timer )*1000 + 1), 1))
+
+    print("Average frame time:", np.average(times))
+    # out.release()
+    streamer.stop()
+    cv2.destroyAllWindows()
+
+main()
