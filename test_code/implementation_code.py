f9ffb2596677862e3c51d7c1b229916f7a9e2b9c
Spencer Teetaert <45899408+spencerteetaert@users.noreply.github.com>
2020-06-10 15:32:30 -0500

    Continued development on multiprocessing conversion

--- test_code/implementation_code.py
+++ test_code/implementation_code.py
@@ -1,8 +1,10 @@
 import time
 import multiprocessing as mp
+from queue import Queue
 
 import numpy as np 
 import cv2
+from pylogix import PLC
 
 from context import source
 from source.vision_identification.video_reader import FileVideoStream
@@ -18,12 +20,14 @@ from source.global_parameters import global_parameters
 '''
 
 DATA_PATH = r"C:\Users\User\Documents\Hylife 2020\Loin Feeder\Data\good.mp4"
+instruction_handler = Queue(maxsize=1048)#InstructionHandler() 
 
 def on_mouse(event, pX, pY, flags, param):
     if event == cv2.EVENT_LBUTTONUP:
         print("Clicked", pX, pY)
 
 def main(data_path=DATA_PATH):
+    global instruction_handler
     drawing_model = Robot(global_parameters['ROBOT_BASE_POINT'], global_parameters['VIDEO_SCALE'])
 
     frame_handler = FrameHandler()
@@ -84,12 +88,18 @@ def main(data_path=DATA_PATH):
                             time_stamps, profiles = frame_handler.get_results(vel_toggle=False)
 
                             flag = False
+                            currentQ = Queue(maxsize=1048)
                             for i in range(0, len(profiles)):
                                 # Adds full path to instruction handler 
-                                instruction_handler.add(time_stamps[i], profiles[i])
+                                currentQ.put(time_stamps[i])
+                                currentQ.put(profiles[i])
                                 flag = True
                             if flag:
-                                instruction_handler.add(0, 0) # Ending flag. This is how the handler knows the command is over
+                                currentQ.put(0)
+                                currentQ.put(0) # Ending flag. This is how the handler knows the command is over
+
+                                process = mp.Process(target=run(currentQ))
+                                process.start()
                     
         delay += 1
 
@@ -97,7 +107,7 @@ def main(data_path=DATA_PATH):
         ### Display ###
         ############### 
         
-        drawing_model.set_model_state(instruction_handler.to_send)
+        # drawing_model.set_model_state(instruction_handler.to_send)
 
         # if DISPLAY_TOGGLE:
         if timer1 > 0:
@@ -135,11 +145,52 @@ def main(data_path=DATA_PATH):
     streamer.stop()
     cv2.destroyAllWindows()
 
-if __name__=="__main__":
+def run(currentQ):    
+    with PLC() as plc:
+        plc.IPAddress = global_parameters['PLC_IP']
+        current_time = currentQ.get()
+        counter = 0
 
-    instruction_handler = InstructionHandler() 
-    # instruction_handler.start()
-    process = mp.Process(target=instruction_handler.run)
-    process.start()
-    
+        while True:
+            if current_time < time.time() and current_time > 0:
+                if counter == 0:
+                    s = time.time()
+                    print("Sending instructions to PLC...")
+                counter += 1
+                if not currentQ.empty():
+                    instruction = currentQ.get()
+                    to_send = np.around(instruction, 2)
+
+                    ### PLC write instruction ###
+                    print(instruction)
+                    # plc.Write("<tag0>", value=instruction[0])
+                    # plc.Write("<tag1>", value=instruction[1])
+                    # plc.Write("<tag2>", value=instruction[2])
+                    # plc.Write("<tag3>", value=instruction[3])
+                    # plc.Write("<tag4>", value=instruction[4])
+                    # plc.Write("<tag5>", value=instruction[5])
+                    # plc.Write("<tag6>", value=instruction[6])
+                    # plc.Write("<tag7>", value=instruction[7])
+                    #############################
+
+                    current_time = currentQ.get()
+                else:
+                    print("ERROR: Instruction size mismatch.")
+
+            elif current_time == 0 and counter != 0:
+
+                # Send a stop order to stop all motion
+                # plc.Write("<tag0>", value=instruction[0])
+                # plc.Write("<tag1>", value=instruction[1])
+                # plc.Write("<tag2>", value=instruction[2])
+                # plc.Write("<tag3>", value=instruction[3])
+                # plc.Write("<tag4>", value=instruction[4])
+                # plc.Write("<tag5>", value=instruction[5])
+                # plc.Write("<tag6>", value=instruction[6])
+                # plc.Write("<tag7>", value=instruction[7])
+
+                print("Instruction completed. \nExecution time:", round(time.time() - s, 2),"s\n")
+                break
+
+if __name__=="__main__":
     main()
